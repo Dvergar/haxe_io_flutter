3fe8edecf8cacc8985e3bc273c63a8ef68821bba
Dvergar <laurent.rd@gmail.com>
2020-05-07 03:19:32 +0200

    css to stylesheet.dart

---
+++ lib/stylesheet.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const aColor = Color.fromARGB(255, 29, 161, 242);
+const aBackgroundColor = Color.fromRGBO(29, 161, 242, 0.05);
+const double pFontSize = 18;
